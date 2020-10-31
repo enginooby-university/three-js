01aba792967bb0e163194b7ada2ddf396b6dc8aa
Hieu Ngo <justacpulover@gmail.com>
2020-10-31 11:17:12 +0100

    Refactor

--- dist/client/tasks/newton_cradle.js
+++ dist/client/tasks/newton_cradle.js
@@ -22,9 +22,10 @@ const options = {
         "Dust": "dust",
     }
 };
+const BALL_AMOUNT = 3;
 const BALL_RADIUS = 0.5;
 const sphereGeometry = new THREE.SphereGeometry(BALL_RADIUS, 64, 64);
-const physicalMaterial = new THREE.MeshPhysicalMaterial({});
+const ballMaterial = new THREE.MeshPhysicalMaterial({});
 let firstBall;
 let lastBall;
 const barGeometry = new THREE.BoxGeometry(10, 0.2, 0.2);
@@ -39,8 +40,8 @@ const ROPE_LENGHT = 3.5;
 const ROPE_TO_FLOOR = 5;
 let firstRope;
 let lastRope;
-let ropeGeometry = new THREE.CylinderGeometry(0.03, 0.03, ROPE_LENGHT);
-let ropeMaterial = new THREE.MeshBasicMaterial({ color: 0xA52A2A });
+const ropeGeometry = new THREE.CylinderGeometry(0.03, 0.03, ROPE_LENGHT);
+const ropeMaterial = new THREE.MeshBasicMaterial({ color: 0xA52A2A });
 const ROTATE_SPEED = 0.03;
 const MAX_ANGLE = 0.7;
 let firstRopeRotateVel;
@@ -48,55 +49,18 @@ let lastRopeRotateVel;
 init();
 export function init() {
     generateSkybox();
-    physicalMaterial.metalness = 1;
-    physicalMaterial.roughness = 0.6;
-    physicalMaterial.transparent = true;
-    barMaterial.metalness = 1;
-    barMaterial.roughness = 0.6;
-    barMaterial.transparent = true;
-    createBar(0, 5, 0);
-    const leftBar = createBar(0, 0, 0);
-    leftBar.rotation.z = Math.PI / 2;
-    leftBar.position.x = -4.90;
-    leftBar.position.y = 2.5;
-    leftBar.scale.x = 0.5;
-    const rightBar = createBar(0, 0, 0);
-    rightBar.rotation.z = Math.PI / 2;
-    rightBar.position.x = 4.90;
-    rightBar.position.y = 2.5;
-    rightBar.scale.x = 0.5;
-    ropeGeometry.translate(0, -ROPE_LENGHT / 2, 0);
-    firstBall = createBall(-(BALL_RADIUS * 2), 1, 0);
-    createBall(0, 1, 0);
-    lastBall = createBall(BALL_RADIUS * 2, 1, 0);
-    firstRope = createRope(-(BALL_RADIUS * 2), ROPE_TO_FLOOR, 0);
-    createRope(0, ROPE_TO_FLOOR, 0);
-    lastRope = createRope((BALL_RADIUS * 2), ROPE_TO_FLOOR, 0);
-    firstRope.rotation.z = -MAX_ANGLE;
-    firstRopeRotateVel = ROTATE_SPEED;
-    lastRopeRotateVel = 0;
-    directionalLight.position.set(4.5, 21, 13);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.mapSize.width = 2048;
-    directionalLight.shadow.mapSize.height = 2048;
-    directionalLight.shadow.camera.near = 0.5;
-    directionalLight.shadow.camera.far = 100;
-    directionalLight.shadow.camera.rotation.x = Math.PI / 2;
-    scene.add(directionalLight);
-    scene.add(directionalLightHelper);
-    scene.add(lightShadowHelper);
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotateX(-Math.PI / 2);
-    plane.position.y = -0.01;
-    plane.receiveShadow = true;
-    scene.add(plane);
+    createBalls(BALL_AMOUNT);
+    createRopes(BALL_AMOUNT);
+    createBars();
+    createLight();
+    createFloor();
 }
 export function createDatGUI() {
     gui = new GUI();
     gui.add(Name, 'Skybox', options.skybox).onChange(() => generateSkybox());
     DatHelper.createDirectionalLightFolder(gui, directionalLight);
     const ballFolder = gui.addFolder("Balls");
-    DatHelper.createPhysicalMaterialFolder(ballFolder, physicalMaterial);
+    DatHelper.createPhysicalMaterialFolder(ballFolder, ballMaterial);
     const ropeFolder = gui.addFolder("Ropes");
     DatHelper.createMaterialFolder(ropeFolder, ropeMaterial);
     const barFolder = gui.addFolder("Bars");
@@ -126,19 +90,76 @@ export function render() {
             lastRopeRotateVel = -ROTATE_SPEED;
         }
     }
+    // update first and last rope rotations
     firstRope.rotation.z += firstRopeRotateVel;
     lastRope.rotation.z += lastRopeRotateVel;
-    firstBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(firstRope.rotation.z) - BALL_RADIUS * 2; // original x of first ball
+    // update first and last ball positions
+    firstBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(firstRope.rotation.z) + BALL_RADIUS * (1 - BALL_AMOUNT + 2 * 0); // original x of first ball
     firstBall.position.y = ROPE_TO_FLOOR - (ROPE_LENGHT + BALL_RADIUS) * Math.cos(firstRope.rotation.z);
-    lastBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(lastRope.rotation.z) + BALL_RADIUS * 2;
+    lastBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(lastRope.rotation.z) + BALL_RADIUS * (1 - BALL_AMOUNT + 2 * (BALL_AMOUNT - 1)); // original x of last ball
     lastBall.position.y = ROPE_TO_FLOOR - (ROPE_LENGHT + BALL_RADIUS) * Math.cos(lastRope.rotation.z);
 }
+function createLight() {
+    directionalLight.position.set(4.5, 21, 13);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 100;
+    directionalLight.shadow.camera.rotation.x = Math.PI / 2;
+    scene.add(directionalLight);
+    scene.add(directionalLightHelper);
+    scene.add(lightShadowHelper);
+}
+function createFloor() {
+    plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotateX(-Math.PI / 2);
+    plane.position.y = -0.01;
+    plane.receiveShadow = true;
+    scene.add(plane);
+}
+function createBalls(amount) {
+    const balls = new THREE.Group();
+    ballMaterial.metalness = 1;
+    ballMaterial.roughness = 0.6;
+    ballMaterial.transparent = true;
+    for (let i = 0; i < amount; i++) {
+        const xBall = BALL_RADIUS * (1 - amount + 2 * i);
+        if (i == 0) {
+            firstBall = createBall(xBall, 1, 0);
+        }
+        else if (i == amount - 1) {
+            lastBall = createBall(xBall, 1, 0);
+        }
+        else {
+            createBall(xBall, 1, 0);
+        }
+    }
+}
 function createBall(x, y, z) {
-    const newBall = new THREE.Mesh(sphereGeometry, physicalMaterial);
+    const newBall = new THREE.Mesh(sphereGeometry, ballMaterial);
     newBall.position.set(x, y, z);
     newBall.castShadow = true;
     scene.add(newBall);
     return newBall;
+}
+function createRopes(amount) {
+    ropeGeometry.translate(0, -ROPE_LENGHT / 2, 0);
+    for (let i = 0; i < amount; i++) {
+        const xRope = BALL_RADIUS * (1 - amount + 2 * i);
+        if (i == 0) {
+            firstRope = createRope(xRope, ROPE_TO_FLOOR, 0);
+        }
+        else if (i == amount - 1) {
+            lastRope = createRope(xRope, ROPE_TO_FLOOR, 0);
+        }
+        else {
+            createRope(xRope, ROPE_TO_FLOOR, 0);
+        }
+    }
+    firstRope.rotation.z = -MAX_ANGLE;
+    firstRopeRotateVel = ROTATE_SPEED;
+    lastRopeRotateVel = 0;
 }
 function createRope(x, y, z) {
     const newRope = new THREE.Mesh(ropeGeometry, ropeMaterial);
@@ -146,6 +167,22 @@ function createRope(x, y, z) {
     newRope.castShadow = true;
     scene.add(newRope);
     return newRope;
+}
+function createBars() {
+    barMaterial.metalness = 1;
+    barMaterial.roughness = 0.6;
+    barMaterial.transparent = true;
+    createBar(0, 5, 0);
+    const leftBar = createBar(0, 0, 0);
+    leftBar.rotation.z = Math.PI / 2;
+    leftBar.position.x = -4.90;
+    leftBar.position.y = 2.5;
+    leftBar.scale.x = 0.5;
+    const rightBar = createBar(0, 0, 0);
+    rightBar.rotation.z = Math.PI / 2;
+    rightBar.position.x = 4.90;
+    rightBar.position.y = 2.5;
+    rightBar.scale.x = 0.5;
 }
 function createBar(x, y, z) {
     const newBar = new THREE.Mesh(barGeometry, barMaterial);
--- src/client/tasks/newton_cradle.ts
+++ src/client/tasks/newton_cradle.ts
@@ -29,9 +29,10 @@ const options = {
     }
 }
 
+const BALL_AMOUNT: number = 3
 const BALL_RADIUS: number = 0.5
 const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(BALL_RADIUS, 64, 64)
-const physicalMaterial: THREE.MeshPhysicalMaterial = new THREE.MeshPhysicalMaterial({})
+const ballMaterial: THREE.MeshPhysicalMaterial = new THREE.MeshPhysicalMaterial({})
 let firstBall: THREE.Mesh
 let lastBall: THREE.Mesh
 
@@ -50,8 +51,8 @@ const ROPE_LENGHT: number = 3.5
 const ROPE_TO_FLOOR: number = 5
 let firstRope: THREE.Mesh
 let lastRope: THREE.Mesh
-let ropeGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(0.03, 0.03, ROPE_LENGHT)
-let ropeMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xA52A2A })
+const ropeGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(0.03, 0.03, ROPE_LENGHT)
+const ropeMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xA52A2A })
 
 const ROTATE_SPEED: number = 0.03
 const MAX_ANGLE: number = 0.7
@@ -62,57 +63,11 @@ init()
 
 export function init() {
     generateSkybox()
-
-    physicalMaterial.metalness = 1
-    physicalMaterial.roughness = 0.6
-    physicalMaterial.transparent = true
-
-    barMaterial.metalness = 1
-    barMaterial.roughness = 0.6
-    barMaterial.transparent = true
-    createBar(0, 5, 0)
-    const leftBar = createBar(0, 0, 0)
-    leftBar.rotation.z = Math.PI / 2
-    leftBar.position.x = -4.90
-    leftBar.position.y = 2.5
-    leftBar.scale.x = 0.5
-    const rightBar = createBar(0, 0, 0)
-    rightBar.rotation.z = Math.PI / 2
-    rightBar.position.x = 4.90
-    rightBar.position.y = 2.5
-    rightBar.scale.x = 0.5
-
-    ropeGeometry.translate(0, -ROPE_LENGHT / 2, 0)
-
-    firstBall = createBall(-(BALL_RADIUS * 2), 1, 0)
-    createBall(0, 1, 0)
-    lastBall = createBall(BALL_RADIUS * 2, 1, 0)
-
-    firstRope = createRope(-(BALL_RADIUS * 2), ROPE_TO_FLOOR, 0)
-    createRope(0, ROPE_TO_FLOOR, 0)
-    lastRope = createRope((BALL_RADIUS * 2), ROPE_TO_FLOOR, 0)
-
-    firstRope.rotation.z = -MAX_ANGLE
-    firstRopeRotateVel = ROTATE_SPEED
-    lastRopeRotateVel = 0
-
-    directionalLight.position.set(4.5, 21, 13)
-    directionalLight.castShadow = true;
-    directionalLight.shadow.mapSize.width = 2048
-    directionalLight.shadow.mapSize.height = 2048
-    directionalLight.shadow.camera.near = 0.5;
-    directionalLight.shadow.camera.far = 100
-    directionalLight.shadow.camera.rotation.x = Math.PI / 2
-
-    scene.add(directionalLight)
-    scene.add(directionalLightHelper)
-    scene.add(lightShadowHelper);
-
-    plane = new THREE.Mesh(planeGeometry, planeMaterial)
-    plane.rotateX(-Math.PI / 2)
-    plane.position.y = -0.01
-    plane.receiveShadow = true;
-    scene.add(plane)
+    createBalls(BALL_AMOUNT)
+    createRopes(BALL_AMOUNT)
+    createBars()
+    createLight()
+    createFloor()
 }
 
 export function createDatGUI() {
@@ -120,7 +75,7 @@ export function createDatGUI() {
     gui.add(Name, 'Skybox', options.skybox).onChange(() => generateSkybox())
     DatHelper.createDirectionalLightFolder(gui, directionalLight)
     const ballFolder: GUI = gui.addFolder("Balls")
-    DatHelper.createPhysicalMaterialFolder(ballFolder, physicalMaterial)
+    DatHelper.createPhysicalMaterialFolder(ballFolder, ballMaterial)
     const ropeFolder: GUI = gui.addFolder("Ropes")
     DatHelper.createMaterialFolder(ropeFolder, ropeMaterial)
     const barFolder: GUI = gui.addFolder("Bars")
@@ -155,22 +110,83 @@ export function render() {
         }
     }
 
+    // update first and last rope rotations
     firstRope.rotation.z += firstRopeRotateVel
     lastRope.rotation.z += lastRopeRotateVel
 
-    firstBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(firstRope.rotation.z) - BALL_RADIUS * 2 // original x of first ball
+    // update first and last ball positions
+    firstBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(firstRope.rotation.z) + BALL_RADIUS * (1 - BALL_AMOUNT + 2 * 0) // original x of first ball
     firstBall.position.y = ROPE_TO_FLOOR - (ROPE_LENGHT + BALL_RADIUS) * Math.cos(firstRope.rotation.z)
-    lastBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(lastRope.rotation.z) + BALL_RADIUS * 2
+    lastBall.position.x = (ROPE_LENGHT + BALL_RADIUS) * Math.sin(lastRope.rotation.z) + BALL_RADIUS * (1 - BALL_AMOUNT + 2 * (BALL_AMOUNT - 1)) // original x of last ball
     lastBall.position.y = ROPE_TO_FLOOR - (ROPE_LENGHT + BALL_RADIUS) * Math.cos(lastRope.rotation.z)
 }
 
+function createLight() {
+    directionalLight.position.set(4.5, 21, 13)
+    directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048
+    directionalLight.shadow.mapSize.height = 2048
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 100
+    directionalLight.shadow.camera.rotation.x = Math.PI / 2
+
+    scene.add(directionalLight)
+    scene.add(directionalLightHelper)
+    scene.add(lightShadowHelper);
+}
+
+function createFloor() {
+    plane = new THREE.Mesh(planeGeometry, planeMaterial)
+    plane.rotateX(-Math.PI / 2)
+    plane.position.y = -0.01
+    plane.receiveShadow = true;
+    scene.add(plane)
+}
+
+function createBalls(amount: number) {
+    const balls: THREE.Group = new THREE.Group()
+    ballMaterial.metalness = 1
+    ballMaterial.roughness = 0.6
+    ballMaterial.transparent = true
+
+    for (let i = 0; i < amount; i++) {
+        const xBall: number = BALL_RADIUS * (1 - amount + 2 * i)
+        if (i == 0) {
+            firstBall = createBall(xBall, 1, 0)
+        } else if (i == amount - 1) {
+            lastBall = createBall(xBall, 1, 0)
+        } else {
+            createBall(xBall, 1, 0)
+        }
+    }
+}
+
 function createBall(x: number, y: number, z: number) {
-    const newBall: THREE.Mesh = new THREE.Mesh(sphereGeometry, physicalMaterial)
+    const newBall: THREE.Mesh = new THREE.Mesh(sphereGeometry, ballMaterial)
     newBall.position.set(x, y, z)
     newBall.castShadow = true
     scene.add(newBall)
 
     return newBall
+}
+
+function createRopes(amount: number) {
+    ropeGeometry.translate(0, -ROPE_LENGHT / 2, 0)
+
+    for (let i = 0; i < amount; i++) {
+        const xRope = BALL_RADIUS * (1 - amount + 2 * i)
+        if (i == 0) {
+            firstRope = createRope(xRope, ROPE_TO_FLOOR, 0)
+        } else if (i == amount - 1) {
+            lastRope = createRope(xRope, ROPE_TO_FLOOR, 0)
+        } else {
+            createRope(xRope, ROPE_TO_FLOOR, 0)
+        }
+    }
+
+    firstRope.rotation.z = -MAX_ANGLE
+    firstRopeRotateVel = ROTATE_SPEED
+    lastRopeRotateVel = 0
 }
 
 function createRope(x: number, y: number, z: number) {
@@ -180,6 +196,23 @@ function createRope(x: number, y: number, z: number) {
     scene.add(newRope)
 
     return newRope
+}
+
+function createBars() {
+    barMaterial.metalness = 1
+    barMaterial.roughness = 0.6
+    barMaterial.transparent = true
+    createBar(0, 5, 0)
+    const leftBar = createBar(0, 0, 0)
+    leftBar.rotation.z = Math.PI / 2
+    leftBar.position.x = -4.90
+    leftBar.position.y = 2.5
+    leftBar.scale.x = 0.5
+    const rightBar = createBar(0, 0, 0)
+    rightBar.rotation.z = Math.PI / 2
+    rightBar.position.x = 4.90
+    rightBar.position.y = 2.5
+    rightBar.scale.x = 0.5
 }
 
 function createBar(x: number, y: number, z: number) {
